287a497f0171e168133cfa3fa65df34d13935e9a
Aryan Blaauw <ablaauw@gmx.net>
2024-02-24 15:03:53 +0100

    Update 5.sh

--- runfiles/5.sh
+++ runfiles/5.sh
@@ -7,11 +7,11 @@
 
 # docker run -it --privileged twobombs/twobombs /buildall.sh
 
-cd thereminq/Dockerfiles
+cd thereminq
 
-docker build --no-cache -f Dockerfile . -t twobombs/thereminq > thereminq &
-docker build --no-cache -f Dockerfile-controller . -t twobombs/thereminq:controller > thereminq:controller &
-docker build --no-cache -f Dockerfile-controller-all . -t twobombs/thereminq:controller-all > thereminq:controller-all &
-docker build --no-cache -f Dockerfile-cuquantum . -t twobombs/thereminq:cuquantum > thereminq:cuquantum &
+docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq > thereminq &
+docker build --no-cache -f Dockerfiles/Dockerfile-controller . -t twobombs/thereminq:controller > thereminq:controller &
+docker build --no-cache -f Dockerfiles/Dockerfile-controller-all . -t twobombs/thereminq:controller-all > thereminq:controller-all &
+docker build --no-cache -f Dockerfiles/Dockerfile-cuquantum . -t twobombs/thereminq:cuquantum > thereminq:cuquantum &
 
 tail -f /dev/null
