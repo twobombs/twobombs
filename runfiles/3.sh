7c79f4a77aa8c22d50840bcde14614e975974f5b
Aryan Blaauw <ablaauw@gmx.net>
2024-02-24 20:18:09 +0100

    Update 3.sh

--- runfiles/3.sh
+++ runfiles/3.sh
@@ -9,11 +9,11 @@
 
 cd qrackmin
 
-docker build --no-cache -f dockerfiles/Dockerfile . -t twobombs/qrackmin > qrackmin &
-docker build --no-cache -f dockerfiles/Dockerfile-1804 . -t twobombs/qrackmin:1804 > qrackmin:1804 &
-docker build --no-cache -f dockerfiles/Dockerfile-2004 . -t twobombs/qrackmin:2004 > qrackmin:2004 &
-docker build --no-cache -f dockerfiles/Dockerfile-2204 . -t twobombs/qrackmin:2204 > qrackmin:2204 &
-docker build --no-cache -f dockerfiles/Dockerfile-aws . -t twobombs/qrackmin:aws > qrackmin:aws &
-docker build --no-cache -f dockerfiles/Dockerfile-braket . -t twobombs/qrackmin:braket > qrackmin:braket &
+docker build --no-cache -f dockerfiles/Dockerfile . -t twobombs/qrackmin > qrackmin
+docker build --no-cache -f dockerfiles/Dockerfile-1804 . -t twobombs/qrackmin:1804 > qrackmin:1804
+docker build --no-cache -f dockerfiles/Dockerfile-2004 . -t twobombs/qrackmin:2004 > qrackmin:2004
+docker build --no-cache -f dockerfiles/Dockerfile-2204 . -t twobombs/qrackmin:2204 > qrackmin:2204
+docker build --no-cache -f dockerfiles/Dockerfile-aws . -t twobombs/qrackmin:aws > qrackmin:aws
+docker build --no-cache -f dockerfiles/Dockerfile-braket . -t twobombs/qrackmin:braket > qrackmin:braket
 
 tail -f /dev/null
