acd42521cb5345d390fadacc0ed7878b29081684
Aryan Blaauw <ablaauw@gmx.net>
2024-02-24 17:01:34 +0100

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -7,7 +7,7 @@
 
 # setup registry and registry UI
 docker run -d -p 5000:5000 --restart=always --name registry2 registry:2
-docker run -d -p 80:80 -e REGISTRY_URL=$HOSTNAME:5000 joxit/docker-registry-ui:main
+docker run -d -p 80:80 -e REGISTRY_URL=http://$HOSTNAME:5000 joxit/docker-registry-ui:main
 
 # run all 10 jobs dind
 docker run -d --privileged --net=host twobombs/twobombs bash 1.sh
