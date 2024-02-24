7674262225d4254708a5769e5f24c735e2d1fc77
Aryan Blaauw <ablaauw@gmx.net>
2024-02-24 21:24:24 +0100

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -7,8 +7,8 @@
 # docker run -ti --privileged --net=host twobombs/twobombs bash buildall.sh
 
 # setup registry and registry UI
-docker run -d -p 5000:5000 --net:host --restart=always --name registry2 registry:2
-docker run -d -p 80:80 --net:host -e REGISTRY_URL=http://$HOSTNAME:5000 joxit/docker-registry-ui:main
+docker run -d -p 5000:5000 --net=host --restart=always --name registry2 registry:2
+docker run -d -p 80:80 --net=host -e REGISTRY_URL=http://$HOSTNAME:5000 joxit/docker-registry-ui:main
 
 # run all 10 jobs dind
 docker run -d --privileged --net=host twobombs/twobombs bash 1.sh
