41a6e0ddec79afb3308c8702ddecea14a588a2f9
Aryan Blaauw <ablaauw@gmx.net>
2024-02-24 20:17:42 +0100

    Update 10.sh

--- runfiles/10.sh
+++ runfiles/10.sh
@@ -9,11 +9,11 @@
 
 cd thereminq-llama
 
-docker build --no-cache -f Dockerfiles/Dockerfile-elastic . -t twobombs/thereminq-llama:elastic > thereminq-llama:elastic &
-docker build --no-cache -f Dockerfiles/Dockerfile-langchain . -t twobombs/thereminq-llama:langchain > thereminq-llama:langchain &
-docker build --no-cache -f Dockerfiles/Dockerfile-opencl . -t twobombs/thereminq-llama:opencl > thereminq-llama:opencl &
-docker build --no-cache -f Dockerfiles/Dockerfile-orchestrate . -t twobombs/thereminq-llama:orchestrate > thereminq-llama:orchestrate &
-docker build --no-cache -f Dockerfiles/Dockerfile-spark . -t twobombs/thereminq-llama:spark > thereminq-llama:spark &
-docker build --no-cache -f Dockerfiles/Dockerfile-unittest . -t twobombs/thereminq-llama:unittest > thereminq-llama:unittest &
+docker build --no-cache -f Dockerfiles/Dockerfile-elastic . -t twobombs/thereminq-llama:elastic > thereminq-llama:elastic
+docker build --no-cache -f Dockerfiles/Dockerfile-langchain . -t twobombs/thereminq-llama:langchain > thereminq-llama:langchain
+docker build --no-cache -f Dockerfiles/Dockerfile-opencl . -t twobombs/thereminq-llama:opencl > thereminq-llama:opencl
+docker build --no-cache -f Dockerfiles/Dockerfile-orchestrate . -t twobombs/thereminq-llama:orchestrate > thereminq-llama:orchestrate
+docker build --no-cache -f Dockerfiles/Dockerfile-spark . -t twobombs/thereminq-llama:spark > thereminq-llama:spark
+docker build --no-cache -f Dockerfiles/Dockerfile-unittest . -t twobombs/thereminq-llama:unittest > thereminq-llama:unittest
 
 tail -f /dev/null
