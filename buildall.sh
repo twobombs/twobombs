c335719fd7f99e23447c8d1d3343d4ac15a7b968
Aryan Blaauw <ablaauw@gmx.net>
2024-02-24 18:58:35 +0000

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -2,12 +2,13 @@
 # waterfall build this
 # will eat your machine
 # 
-# runs on dind ubuntu version 
-# https://github.com/cruizba/ubuntu-dind 
+# runs on dind ubuntu version - https://github.com/cruizba/ubuntu-dind 
+# run with 
+# docker run -ti --privileged --net:host twobombs/twobombs bash buildall.sh
 
 # setup registry and registry UI
-docker run -d -p 5000:5000 --restart=always --name registry2 registry:2
-docker run -d -p 80:80 -e REGISTRY_URL=http://$HOSTNAME:5000 joxit/docker-registry-ui:main
+docker run -d -p 5000:5000 --net:host --restart=always --name registry2 registry:2
+docker run -d -p 80:80 --net:host -e REGISTRY_URL=http://$HOSTNAME:5000 joxit/docker-registry-ui:main
 
 # run all 10 jobs dind
 docker run -d --privileged --net=host twobombs/twobombs bash 1.sh
