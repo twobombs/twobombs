fde052a9e9f784f20d25643056c50abdf2c20b00
Aryan Blaauw <ablaauw@gmx.net>
2024-02-24 15:03:20 +0100

    Update 4.sh

--- runfiles/4.sh
+++ runfiles/4.sh
@@ -7,14 +7,14 @@
 
 # docker run -it --privileged twobombs/twobombs /buildall.sh
 
-cd qrackmin/dockerfiles
+cd qrackmin
 
-docker build --no-cache -f Dockerfile-cuda . -t twobombs/qrackmin:cuda > qrackmin:cuda &
-docker build --no-cache -f Dockerfile-pocl . -t twobombs/qrackmin:pocl > qrackmin:pocl &
-docker build --no-cache -f Dockerfile-pyqrack . -t twobombs/qrackmin:pyqrack > qrackmin:pyqrack &
-docker build --no-cache -f Dockerfile-qiskit . -t twobombs/qrackmin:qiskit > qrackmin:qiskit &
-docker build --no-cache -f Dockerfile-vcl . -t twobombs/qrackmin:vcl > qrackmin:vcl &
-docker build --no-cache -f Dockerfile-vcl-controller . -t twobombs/qrackmin:vcl-controller > qrackmin:vcl-controller &
-docker build --no-cache -f Dockerfile-vcl-pocl . -t twobombs/qrackmin:vcl-pocl > qrackmin:vcl-controller &
+docker build --no-cache -f dockerfiles/Dockerfile-cuda . -t twobombs/qrackmin:cuda > qrackmin:cuda &
+docker build --no-cache -f dockerfiles/Dockerfile-pocl . -t twobombs/qrackmin:pocl > qrackmin:pocl &
+docker build --no-cache -f dockerfiles/Dockerfile-pyqrack . -t twobombs/qrackmin:pyqrack > qrackmin:pyqrack &
+docker build --no-cache -f dockerfiles/Dockerfile-qiskit . -t twobombs/qrackmin:qiskit > qrackmin:qiskit &
+docker build --no-cache -f dockerfiles/Dockerfile-vcl . -t twobombs/qrackmin:vcl > qrackmin:vcl &
+docker build --no-cache -f dockerfiles/Dockerfile-vcl-controller . -t twobombs/qrackmin:vcl-controller > qrackmin:vcl-controller &
+docker build --no-cache -f dockerfiles/Dockerfile-vcl-pocl . -t twobombs/qrackmin:vcl-pocl > qrackmin:vcl-controller &
 
 tail -f /dev/null
