8653eb10ed76c2267416679aea919e035020ba0f
Aryan Blaauw <ablaauw@gmx.net>
2024-02-24 15:06:06 +0100

    Update 9.sh

--- runfiles/9.sh
+++ runfiles/9.sh
@@ -7,12 +7,12 @@
 
 # docker run -it --privileged twobombs/twobombs /buildall.sh
 
-cd thereminq-llama/Dockerfiles
+cd thereminq-llama
 
-docker build --no-cache -f Dockerfile . -t twobombs/thereminq-llama > thereminq-llama &
-docker build --no-cache -f Dockerfile-cassandra . -t twobombs/thereminq-llama:cassandra > thereminq-llama:cassandra &
-docker build --no-cache -f Dockerfile-chatui . -t twobombs/thereminq-llama:chatui > thereminq-llama:chatui &
-docker build --no-cache -f Dockerfile-cli . -t twobombs/thereminq-llama:cli > thereminq-llama:cli &
-docker build --no-cache -f Dockerfile-cuda . -t twobombs/thereminq-llama:cuda > thereminq-llama:cuda &
+docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-llama > thereminq-llama &
+docker build --no-cache -f Dockerfiles/Dockerfile-cassandra . -t twobombs/thereminq-llama:cassandra > thereminq-llama:cassandra &
+docker build --no-cache -f Dockerfiles/Dockerfile-chatui . -t twobombs/thereminq-llama:chatui > thereminq-llama:chatui &
+docker build --no-cache -f Dockerfiles/Dockerfile-cli . -t twobombs/thereminq-llama:cli > thereminq-llama:cli &
+docker build --no-cache -f Dockerfiles/Dockerfile-cuda . -t twobombs/thereminq-llama:cuda > thereminq-llama:cuda &
 
 tail -f /dev/null
