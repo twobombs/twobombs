b7bcf2fcc37d77b52b8f74ba10b043eb5641d7b5
Aryan Blaauw <ablaauw@gmx.net>
2024-02-24 20:19:09 +0100

    Update 8.sh

--- runfiles/8.sh
+++ runfiles/8.sh
@@ -9,10 +9,10 @@
 
 cd thereminq-tensors
 
-docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-tensor > thereminq-tensor &
-docker build --no-cache -f Dockerfiles/Dockerfile-metal . -t twobombs/thereminq-tensor:metal > thereminq-tensor:metal &
-docker build --no-cache -f Dockerfiles/Dockerfile-minimum . -t twobombs/thereminq-tensor:minimum > thereminq-tensor:minimum &
-docker build --no-cache -f Dockerfiles/Dockerfile-quda . -t twobombs/thereminq-tensor:quda > thereminq-tensor:quda &
-docker build --no-cache -f Dockerfiles/Dockerfile-shors . -t twobombs/thereminq-tensor:shors > thereminq-tensor:shors &
+docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-tensor > thereminq-tensor
+docker build --no-cache -f Dockerfiles/Dockerfile-metal . -t twobombs/thereminq-tensor:metal > thereminq-tensor:metal
+docker build --no-cache -f Dockerfiles/Dockerfile-minimum . -t twobombs/thereminq-tensor:minimum > thereminq-tensor:minimum
+docker build --no-cache -f Dockerfiles/Dockerfile-quda . -t twobombs/thereminq-tensor:quda > thereminq-tensor:quda
+docker build --no-cache -f Dockerfiles/Dockerfile-shors . -t twobombs/thereminq-tensor:shors > thereminq-tensor:shors
 
 tail -f /dev/null
