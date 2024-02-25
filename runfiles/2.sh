2ff414bc06056d23c5800279cf0977be2b0ab337
Aryan Blaauw <ablaauw@gmx.net>
2024-02-25 12:12:24 +0100

    Update 2.sh

--- runfiles/2.sh
+++ runfiles/2.sh
@@ -9,5 +9,3 @@ docker build --no-cache -f Dockerfile-hpc . -t twobombs/cudacluster:hpc > cudacl
 docker build --no-cache -f Dockerfile-samples . -t twobombs/cudacluster:samples > cudacluster:samples && docker tag twobombs/cudacluster:samples $HOSTNAME:5000/twobombs/cudacluster:samples && docker push $HOSTNAME:5000/twobombs/cudacluster:samples
 docker build --no-cache -f Dockerfile-vulcan . -t twobombs/cudacluster:vulcan > cudacluster:vulcan && docker tag twobombs/cudacluster:vulcan $HOSTNAME:5000/twobombs/cudacluster:vulcan && docker push $HOSTNAME:5000/twobombs/cudacluster:vulcan
 docker build --no-cache -f Dockerfile-gaming . -t twobombs/cudacluster:gaming > cudacluster:gaming && docker tag twobombs/cudacluster:gaming $HOSTNAME:5000/twobombs/cudacluster:gaming && docker push $HOSTNAME:5000/twobombs/cudacluster:gaming
-
-tail -f /dev/null
