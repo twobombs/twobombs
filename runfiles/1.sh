10f53ca40fa43423b329967711118269816f2473
Aryan Blaauw <ablaauw@gmx.net>
2024-02-25 11:30:16 +0100

    Update 1.sh

--- runfiles/1.sh
+++ runfiles/1.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-# waterfall build this
-# will eat your machine
-# 
-# runs on dind ubuntu version 
-# https://github.com/cruizba/ubuntu-dind 
-
-# docker run -it --privileged twobombs/twobombs /buildall.sh
+# build and push
 
 cd deploy-nvidia-docker
 
-docker build --no-cache -f Dockerfile . -t twobombs/deploy-nvidia-docker > deploy-nvidia-docker
-docker build --no-cache -f Dockerfile-dev . -t twobombs/deploy-nvidia-docker:dev > deploy-nvidia-docker:dev
-docker build --no-cache -f Dockerfile-minimum . -t twobombs/deploy-nvidia-docker:minimum > deploy-nvidia-docker:minimum
+docker build --no-cache -f Dockerfile . -t twobombs/deploy-nvidia-docker > deploy-nvidia-docker && docker tag twobombs/deploy-nvidia-docker $HOSTNAME:5000/twobombs/deploy-nvidia-docker && docker push $HOSTNAME:5000/twobombs/deploy-nvidia-docker
+docker build --no-cache -f Dockerfile-dev . -t twobombs/deploy-nvidia-docker:dev > deploy-nvidia-docker:dev && docker tag twobombs/deploy-nvidia-docker:dev $HOSTNAME:5000/twobombs/deploy-nvidia-docker:dev && docker push $HOSTNAME:5000/twobombs/deploy-nvidia-docker:dev
+docker build --no-cache -f Dockerfile-minimum . -t twobombs/deploy-nvidia-docker:minimum > deploy-nvidia-docker:minimum && docker tag twobombs/deploy-nvidia-docker:minimum $HOSTNAME:5000/twobombs/deploy-nvidia-docker:minimum && docker push $HOSTNAME:5000/twobombs/deploy-nvidia-docker:minimum
 
 tail -f /dev/null
