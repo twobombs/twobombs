8f94647a914b29723cd92b0990284ab251f88a3b
Aryan Blaauw <ablaauw@gmx.net>
2024-02-25 12:07:00 +0100

    Update 9.sh

--- runfiles/9.sh
+++ runfiles/9.sh
@@ -3,10 +3,10 @@
 
 cd thereminq-llama
 
-docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-llama > thereminq-llama && docker tag thereminq-llama $HOSTNAME:5000/thereminq-llama && docker push $HOSTNAME:5000/thereminq-llama
-docker build --no-cache -f Dockerfiles/Dockerfile-cassandra . -t twobombs/thereminq-llama:cassandra > thereminq-llama:cassandra && docker tag thereminq-llama:cassandra $HOSTNAME:5000/thereminq-llama:cassandra && docker push $HOSTNAME:5000/thereminq-llama:cassandra
-docker build --no-cache -f Dockerfiles/Dockerfile-cli . -t twobombs/thereminq-llama:cli > thereminq-llama:cli && docker tag thereminq-llama:cli $HOSTNAME:5000/thereminq-llama:cli && docker push $HOSTNAME:5000/thereminq-llama:cli
-docker build --no-cache -f Dockerfiles/Dockerfile-cuda . -t twobombs/thereminq-llama:cuda > thereminq-llama:cuda && docker tag thereminq-llama:cuda $HOSTNAME:5000/thereminq-llama:cuda && docker push $HOSTNAME:5000/thereminq-llama:cuda
-docker build --no-cache -f Dockerfiles/Dockerfile-chatui . -t twobombs/thereminq-llama:chatui > thereminq-llama:chatui && docker tag thereminq-llama:chatui $HOSTNAME:5000/thereminq-llama:chatui && docker push $HOSTNAME:5000/thereminq-llama:chatui
+docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-llama > thereminq-llama && docker tag twobombs/thereminq-llama $HOSTNAME:5000/twobombs/thereminq-llama && docker push $HOSTNAME:5000/twobombs/thereminq-llama
+docker build --no-cache -f Dockerfiles/Dockerfile-cassandra . -t twobombs/thereminq-llama:cassandra > thereminq-llama:cassandra && docker tag twobombs/thereminq-llama:cassandra $HOSTNAME:5000/twobombs/thereminq-llama:cassandra && docker push $HOSTNAME:5000/twobombs/thereminq-llama:cassandra
+docker build --no-cache -f Dockerfiles/Dockerfile-cli . -t twobombs/thereminq-llama:cli > thereminq-llama:cli && docker tag twobombs/thereminq-llama:cli $HOSTNAME:5000/twobombs/thereminq-llama:cli && docker push $HOSTNAME:5000/twobombs/thereminq-llama:cli
+docker build --no-cache -f Dockerfiles/Dockerfile-cuda . -t twobombs/thereminq-llama:cuda > thereminq-llama:cuda && docker tag twobombs/thereminq-llama:cuda $HOSTNAME:5000/twobombs/thereminq-llama:cuda && docker push $HOSTNAME:5000/twobombs/thereminq-llama:cuda
+docker build --no-cache -f Dockerfiles/Dockerfile-chatui . -t twobombs/thereminq-llama:chatui > thereminq-llama:chatui && docker tag twobombs/thereminq-llama:chatui $HOSTNAME:5000/twobombs/thereminq-llama:chatui && docker push $HOSTNAME:5000/twobombs/thereminq-llama:chatui
 
 tail -f /dev/null
