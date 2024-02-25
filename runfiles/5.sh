a6138ce279671209d65004ade68da14b9fdc8f68
Aryan Blaauw <ablaauw@gmx.net>
2024-02-25 11:43:37 +0100

    Update 5.sh

--- runfiles/5.sh
+++ runfiles/5.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-# waterfall build this
-# will eat your machine
-# 
-# runs on dind ubuntu version 
-# https://github.com/cruizba/ubuntu-dind 
-
-# docker run -it --privileged twobombs/twobombs /buildall.sh
+#
 
 cd thereminq
 
-docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq > thereminq
-docker build --no-cache -f Dockerfiles/Dockerfile-controller . -t twobombs/thereminq:controller > thereminq:controller
-docker build --no-cache -f Dockerfiles/Dockerfile-controller-all . -t twobombs/thereminq:controller-all > thereminq:controller-all
-docker build --no-cache -f Dockerfiles/Dockerfile-cuquantum . -t twobombs/thereminq:cuquantum > thereminq:cuquantum
+docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq > thereminq && docker tag twobombs/thereminq $HOSTNAME:5000/twobombs/thereminq && docker push $HOSTNAME:5000/twobombs/thereminq
+docker build --no-cache -f Dockerfiles/Dockerfile-controller . -t twobombs/thereminq:controller > thereminq:controller && docker tag twobombs/thereminq:controller $HOSTNAME:5000/twobombs/thereminq:controller && docker push $HOSTNAME:5000/twobombs/thereminq:controller
+docker build --no-cache -f Dockerfiles/Dockerfile-controller-all . -t twobombs/thereminq:controller-all > thereminq:controller-all && docker tag twobombs/thereminq:controller-all $HOSTNAME:5000/twobombs/thereminq:controller-all && docker push $HOSTNAME:5000/twobombs/thereminq:controller-all
+docker build --no-cache -f Dockerfiles/Dockerfile-cuquantum . -t twobombs/thereminq:cuquantum > thereminq:cuquantum && docker tag twobombs/thereminq:cuquantum $HOSTNAME:5000/twobombs/thereminq:cuquantum && docker push $HOSTNAME:5000/twobombs/thereminq:cuquantum
 
 tail -f /dev/null
