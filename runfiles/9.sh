ceb36139fbbfb7a7a0ee5fdae09209f775ae6a1a
Aryan Blaauw <ablaauw@gmx.net>
2024-02-25 11:56:55 +0100

    Update 9.sh

--- runfiles/9.sh
+++ runfiles/9.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-# waterfall build this
-# will eat your machine
-# 
-# runs on dind ubuntu version 
-# https://github.com/cruizba/ubuntu-dind 
-
-# docker run -it --privileged twobombs/twobombs /buildall.sh
+#
 
 cd thereminq-llama
 
-docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-llama > thereminq-llama
-docker build --no-cache -f Dockerfiles/Dockerfile-cassandra . -t twobombs/thereminq-llama:cassandra > thereminq-llama:cassandra
-docker build --no-cache -f Dockerfiles/Dockerfile-cli . -t twobombs/thereminq-llama:cli > thereminq-llama:cli
-docker build --no-cache -f Dockerfiles/Dockerfile-cuda . -t twobombs/thereminq-llama:cuda > thereminq-llama:cuda
-docker build --no-cache -f Dockerfiles/Dockerfile-chatui . -t twobombs/thereminq-llama:chatui > thereminq-llama:chatui
+docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-llama > thereminq-llama && docker tag thereminq-llama $HOSTNAME:5000/thereminq-llama && docker push $HOSTNAME:5000/thereminq-llama
+docker build --no-cache -f Dockerfiles/Dockerfile-cassandra . -t twobombs/thereminq-llama:cassandra > thereminq-llama:cassandra && docker tag thereminq-llama:cassandra $HOSTNAME:5000/thereminq-llama:cassandra && docker push $HOSTNAME:5000/thereminq-llama:cassandra
+docker build --no-cache -f Dockerfiles/Dockerfile-cli . -t twobombs/thereminq-llama:cli > thereminq-llama:cli && docker tag thereminq-llama:cli $HOSTNAME:5000/thereminq-llama:cli && docker push $HOSTNAME:5000/thereminq-llama:cli
+docker build --no-cache -f Dockerfiles/Dockerfile-cuda . -t twobombs/thereminq-llama:cuda > thereminq-llama:cuda && docker tag thereminq-llama:cuda $HOSTNAME:5000/thereminq-llama:cuda && docker push $HOSTNAME:5000/thereminq-llama:cuda
+docker build --no-cache -f Dockerfiles/Dockerfile-chatui . -t twobombs/thereminq-llama:chatui > thereminq-llama:chatui && docker tag thereminq-llama:chatui $HOSTNAME:5000/thereminq-llama:chatui && docker push $HOSTNAME:5000/thereminq-llama:chatui
 
 tail -f /dev/null
