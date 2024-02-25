4f8c63755b13972f2f6f720d8eff1b6ba3e2abf6
Aryan Blaauw <ablaauw@gmx.net>
2024-02-25 11:54:56 +0100

    Update 8.sh

--- runfiles/8.sh
+++ runfiles/8.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-# waterfall build this
-# will eat your machine
-# 
-# runs on dind ubuntu version 
-# https://github.com/cruizba/ubuntu-dind 
-
-# docker run -it --privileged twobombs/twobombs /buildall.sh
+#
 
 cd thereminq-tensors
 
-docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-tensor > thereminq-tensor
-docker build --no-cache -f Dockerfiles/Dockerfile-metal . -t twobombs/thereminq-tensor:metal > thereminq-tensor:metal
-docker build --no-cache -f Dockerfiles/Dockerfile-minimum . -t twobombs/thereminq-tensor:minimum > thereminq-tensor:minimum
-docker build --no-cache -f Dockerfiles/Dockerfile-quda . -t twobombs/thereminq-tensor:quda > thereminq-tensor:quda
-docker build --no-cache -f Dockerfiles/Dockerfile-shors . -t twobombs/thereminq-tensor:shors > thereminq-tensor:shors
+docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-tensor > thereminq-tensor && docker tag thereminq-tensor $HOSTNAME:5000/thereminq-tensor && docker push $HOSTNAME:5000/thereminq-tensor
+docker build --no-cache -f Dockerfiles/Dockerfile-metal . -t twobombs/thereminq-tensor:metal > thereminq-tensor:metal && docker tag thereminq-tensor:metal $HOSTNAME:5000/thereminq-tensor:metal && docker push $HOSTNAME:5000/thereminq-tensor:metal
+docker build --no-cache -f Dockerfiles/Dockerfile-minimum . -t twobombs/thereminq-tensor:minimum > thereminq-tensor:minimum && docker tag thereminq-tensor:minimum $HOSTNAME:5000/thereminq-tensor:minimum && docker push $HOSTNAME:5000/thereminq-tensor:minimum
+docker build --no-cache -f Dockerfiles/Dockerfile-quda . -t twobombs/thereminq-tensor:quda > thereminq-tensor:quda && docker tag thereminq-tensor:quda $HOSTNAME:5000/thereminq-tensor:quda && docker push $HOSTNAME:5000/thereminq-tensor:quda
+docker build --no-cache -f Dockerfiles/Dockerfile-shors . -t twobombs/thereminq-tensor:shors > thereminq-tensor:shors && docker tag thereminq-tensor:shors $HOSTNAME:5000/thereminq-tensor:shors && docker push $HOSTNAME:5000/thereminq-tensor:shors
 
 tail -f /dev/null
