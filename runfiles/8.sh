d7d0dcbb730d9a8c3493609a52fe50cd39722263
Aryan Blaauw <ablaauw@gmx.net>
2024-02-25 12:05:32 +0100

    Update 8.sh

--- runfiles/8.sh
+++ runfiles/8.sh
@@ -3,10 +3,10 @@
 
 cd thereminq-tensors
 
-docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-tensor > thereminq-tensor && docker tag thereminq-tensor $HOSTNAME:5000/thereminq-tensor && docker push $HOSTNAME:5000/thereminq-tensor
-docker build --no-cache -f Dockerfiles/Dockerfile-metal . -t twobombs/thereminq-tensor:metal > thereminq-tensor:metal && docker tag thereminq-tensor:metal $HOSTNAME:5000/thereminq-tensor:metal && docker push $HOSTNAME:5000/thereminq-tensor:metal
-docker build --no-cache -f Dockerfiles/Dockerfile-minimum . -t twobombs/thereminq-tensor:minimum > thereminq-tensor:minimum && docker tag thereminq-tensor:minimum $HOSTNAME:5000/thereminq-tensor:minimum && docker push $HOSTNAME:5000/thereminq-tensor:minimum
-docker build --no-cache -f Dockerfiles/Dockerfile-quda . -t twobombs/thereminq-tensor:quda > thereminq-tensor:quda && docker tag thereminq-tensor:quda $HOSTNAME:5000/thereminq-tensor:quda && docker push $HOSTNAME:5000/thereminq-tensor:quda
-docker build --no-cache -f Dockerfiles/Dockerfile-shors . -t twobombs/thereminq-tensor:shors > thereminq-tensor:shors && docker tag thereminq-tensor:shors $HOSTNAME:5000/thereminq-tensor:shors && docker push $HOSTNAME:5000/thereminq-tensor:shors
+docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-tensor > thereminq-tensor && docker tag twobombs/thereminq-tensor $HOSTNAME:5000/twobombs/thereminq-tensor && docker push $HOSTNAME:5000/twobombs/thereminq-tensor
+docker build --no-cache -f Dockerfiles/Dockerfile-metal . -t twobombs/thereminq-tensor:metal > thereminq-tensor:metal && docker tag twobombs/thereminq-tensor:metal $HOSTNAME:5000/twobombs/thereminq-tensor:metal && docker push $HOSTNAME:5000/twobombs/thereminq-tensor:metal
+docker build --no-cache -f Dockerfiles/Dockerfile-minimum . -t twobombs/thereminq-tensor:minimum > thereminq-tensor:minimum && docker tag twobombs/thereminq-tensor:minimum $HOSTNAME:5000/twobombs/thereminq-tensor:minimum && docker push $HOSTNAME:5000/twobombs/thereminq-tensor:minimum
+docker build --no-cache -f Dockerfiles/Dockerfile-quda . -t twobombs/thereminq-tensor:quda > thereminq-tensor:quda && docker tag twobombs/thereminq-tensor:quda $HOSTNAME:5000/twobombs/thereminq-tensor:quda && docker push $HOSTNAME:5000/twobombs/thereminq-tensor:quda
+docker build --no-cache -f Dockerfiles/Dockerfile-shors . -t twobombs/thereminq-tensor:shors > thereminq-tensor:shors && docker tag twobombs/thereminq-tensor:shors $HOSTNAME:5000/twobombs/thereminq-tensor:shors && docker push $HOSTNAME:5000/twobombs/thereminq-tensor:shors
 
 tail -f /dev/null
