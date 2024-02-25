438573c208c4341f7babddb4b715470bf732e241
Aryan Blaauw <ablaauw@gmx.net>
2024-02-25 11:39:41 +0100

    Update 4.sh

--- runfiles/4.sh
+++ runfiles/4.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-# waterfall build this
-# will eat your machine
-# 
-# runs on dind ubuntu version 
-# https://github.com/cruizba/ubuntu-dind 
-
-# docker run -it --privileged twobombs/twobombs /buildall.sh
+#
 
 cd qrackmin
 
-docker build --no-cache -f dockerfiles/Dockerfile-cuda . -t twobombs/qrackmin:cuda > qrackmin:cuda
-docker build --no-cache -f dockerfiles/Dockerfile-pocl . -t twobombs/qrackmin:pocl > qrackmin:pocl
-docker build --no-cache -f dockerfiles/Dockerfile-pyqrack . -t twobombs/qrackmin:pyqrack > qrackmin:pyqrack
-docker build --no-cache -f dockerfiles/Dockerfile-qiskit . -t twobombs/qrackmin:qiskit > qrackmin:qiskit
-docker build --no-cache -f dockerfiles/Dockerfile-vcl . -t twobombs/qrackmin:vcl > qrackmin:vcl
-docker build --no-cache -f dockerfiles/Dockerfile-vcl-controller . -t twobombs/qrackmin:vcl-controller > qrackmin:vcl-controller
-docker build --no-cache -f dockerfiles/Dockerfile-vcl-pocl . -t twobombs/qrackmin:vcl-pocl > qrackmin:vcl-controller
+docker build --no-cache -f dockerfiles/Dockerfile-cuda . -t twobombs/qrackmin:cuda > qrackmin:cuda && docker tag twobombs/qrackmin:cuda $HOSTNAME:5000/twobombs/qrackmin:cuda && docker push $HOSTNAME:5000/twobombs/qrackmin:cuda
+docker build --no-cache -f dockerfiles/Dockerfile-pocl . -t twobombs/qrackmin:pocl > qrackmin:pocl docker tag twobombs/qrackmin:pocl $HOSTNAME:5000/twobombs/qrackmin:pocl && docker push $HOSTNAME:5000/twobombs/qrackmin:pocl
+docker build --no-cache -f dockerfiles/Dockerfile-pyqrack . -t twobombs/qrackmin:pyqrack > qrackmin:pyqrack && docker tag twobombs/qrackmin:pyqrack $HOSTNAME:5000/twobombs/qrackmin:pyqrack && docker push $HOSTNAME:5000/twobombs/qrackmin:pyqrack
+docker build --no-cache -f dockerfiles/Dockerfile-qiskit . -t twobombs/qrackmin:qiskit > qrackmin:qiskit && docker tag twobombs/qrackmin:qiskit $HOSTNAME:5000/twobombs/qrackmin:qiskit && docker push $HOSTNAME:5000/twobombs/qrackmin:qiskit
+docker build --no-cache -f dockerfiles/Dockerfile-vcl . -t twobombs/qrackmin:vcl > qrackmin:vcl && docker tag twobombs/qrackmin:vcl $HOSTNAME:5000/twobombs/qrackmin:vcl && docker push $HOSTNAME:5000/twobombs/qrackmin:vcl
+docker build --no-cache -f dockerfiles/Dockerfile-vcl-controller . -t twobombs/qrackmin:vcl-controller > qrackmin:vcl-controller && docker tag twobombs/qrackmin:vcl-controller $HOSTNAME:5000/twobombs/qrackmin:vcl-controller && docker push $HOSTNAME:5000/twobombs/qrackmin:vcl-controller
+docker build --no-cache -f dockerfiles/Dockerfile-vcl-pocl . -t twobombs/qrackmin:vcl-pocl > qrackmin:vcl-controller && docker tag twobombs/qrackmin:vcl-pocl $HOSTNAME:5000/twobombs/qrackmin:vcl-pocl && docker push $HOSTNAME:5000/twobombs/qrackmin:vcl-pocl
 
 tail -f /dev/null
