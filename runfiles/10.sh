3a59c07fdd052033fc462b707b562009dd2eeaa1
Aryan Blaauw <ablaauw@gmx.net>
2024-02-25 12:13:30 +0100

    Update 10.sh

--- runfiles/10.sh
+++ runfiles/10.sh
@@ -9,5 +9,3 @@ docker build --no-cache -f Dockerfiles/Dockerfile-opencl . -t twobombs/thereminq
 docker build --no-cache -f Dockerfiles/Dockerfile-orchestrate . -t twobombs/thereminq-llama:orchestrate > twobombs/thereminq-llama:orchestrate && docker tag twobombs/thereminq-llama:orchestrate $HOSTNAME:5000/twobombs/thereminq-llama:orchestrate && docker push $HOSTNAME:5000/twobombs/thereminq-llama:orchestrate
 docker build --no-cache -f Dockerfiles/Dockerfile-spark . -t twobombs/thereminq-llama:spark > twobombs/thereminq-llama:spark && docker tag twobombs/thereminq-llama:spark $HOSTNAME:5000/twobombs/thereminq-llama:spark && docker push $HOSTNAME:5000/twobombs/thereminq-llama:spark
 docker build --no-cache -f Dockerfiles/Dockerfile-unittest . -t twobombs/thereminq-llama:unittest > twobombs/thereminq-llama:unittest && docker tag twobombs/thereminq-llama:unittest $HOSTNAME:5000/twobombs/thereminq-llama:unittest && docker push $HOSTNAME:5000/twobombs/thereminq-llama:unittest
-
-tail -f /dev/null
