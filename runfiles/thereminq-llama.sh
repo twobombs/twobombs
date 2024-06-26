9c04dcafc116b82ddf91be639e3e398336bd9ff7
Aryan Blaauw <ablaauw@gmx.net>
2024-06-26 15:30:25 +0000

    Update thereminq-llama.sh

--- runfiles/thereminq-llama.sh
+++ runfiles/thereminq-llama.sh
@@ -12,3 +12,5 @@ docker build --no-cache -f Dockerfiles/Dockerfile-chatui . -t twobombs/thereminq
 docker build --no-cache -f Dockerfiles/Dockerfile-agent . -t twobombs/thereminq-llama:agent > thereminq-llama:agent && docker tag twobombs/thereminq-llama:agent $HOSTNAME:5000/twobombs/thereminq-llama:agent && docker push $HOSTNAME:5000/twobombs/thereminq-llama:agent
 
 docker build --no-cache -f Dockerfiles/Dockerfile-big-agency . -t twobombs/thereminq-llama:big-agency > thereminq-llama:big-agency && docker tag twobombs/thereminq-llama:big-agency $HOSTNAME:5000/twobombs/thereminq-llama:big-agency && docker push $HOSTNAME:5000/twobombs/thereminq-llama:big-agency
+
+docker build --no-cache -f Dockerfiles/Dockerfile-chemistry . -t twobombs/thereminq-llama:chemistry > thereminq-llama:chemistry && docker tag twobombs/thereminq-llama:chemistry $HOSTNAME:5000/twobombs/thereminq-llama:chemistry && docker push $HOSTNAME:5000/twobombs/thereminq-llama:chemistry
