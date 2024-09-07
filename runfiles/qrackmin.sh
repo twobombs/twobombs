e09d28f8faa3d04bf4df8aa502081f7fc402be21
Aryan Blaauw <ablaauw@gmx.net>
2024-09-07 11:38:03 +0200

    Update qrackmin.sh

--- runfiles/qrackmin.sh
+++ runfiles/qrackmin.sh
@@ -13,7 +13,7 @@ docker build --no-cache -f dockerfiles/Dockerfile-braket . -t twobombs/qrackmin:
 docker build --no-cache -f dockerfiles/Dockerfile-cuda . -t twobombs/qrackmin:cuda > qrackmin:cuda && docker tag twobombs/qrackmin:cuda $HOSTNAME:5000/twobombs/qrackmin:cuda && docker push $HOSTNAME:5000/twobombs/qrackmin:cuda
 docker build --no-cache -f dockerfiles/Dockerfile-pocl . -t twobombs/qrackmin:pocl > qrackmin:pocl docker tag twobombs/qrackmin:pocl $HOSTNAME:5000/twobombs/qrackmin:pocl && docker push $HOSTNAME:5000/twobombs/qrackmin:pocl
 docker build --no-cache -f dockerfiles/Dockerfile-pyqrack . -t twobombs/qrackmin:pyqrack > qrackmin:pyqrack && docker tag twobombs/qrackmin:pyqrack $HOSTNAME:5000/twobombs/qrackmin:pyqrack && docker push $HOSTNAME:5000/twobombs/qrackmin:pyqrack
-docker build --no-cache -f dockerfiles/Dockerfile-qimcifa . -t twobombs/qrackmin:qimcifa > qrackmin:qimcifa && docker tag twobombs/qrackmin:qimcifa $HOSTNAME:5000/twobombs/qrackmin:qimcifa && docker push $HOSTNAME:5000/twobombs/qrackmin:qimcifa
+docker build --no-cache -f dockerfiles/Dockerfile-qbdd . -t twobombs/qrackmin:qbdd > qrackmin:qbdd && docker tag twobombs/qrackmin:qbdd $HOSTNAME:5000/twobombs/qrackmin:qbdd && docker push $HOSTNAME:5000/twobombs/qrackmin:qbdd
 
 docker build --no-cache -f dockerfiles/Dockerfile-vcl . -t twobombs/qrackmin:vcl > qrackmin:vcl && docker tag twobombs/qrackmin:vcl $HOSTNAME:5000/twobombs/qrackmin:vcl && docker push $HOSTNAME:5000/twobombs/qrackmin:vcl
 docker build --no-cache -f dockerfiles/Dockerfile-vcl-controller . -t twobombs/qrackmin:vcl-controller > qrackmin:vcl-controller && docker tag twobombs/qrackmin:vcl-controller $HOSTNAME:5000/twobombs/qrackmin:vcl-controller && docker push $HOSTNAME:5000/twobombs/qrackmin:vcl-controller
