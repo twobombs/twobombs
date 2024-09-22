668e4e4e55514f5d4d1f95f1a80fa3af93cae2e2
Aryan Blaauw <ablaauw@gmx.net>
2024-09-22 13:16:21 +0200

    2204

--- runfiles/deploy-nvidia-docker.sh
+++ runfiles/deploy-nvidia-docker.sh
@@ -5,7 +5,12 @@ cd deploy-nvidia-docker
 
 docker build --no-cache -f Dockerfile . -t twobombs/deploy-nvidia-docker > deploy-nvidia-docker && docker tag twobombs/deploy-nvidia-docker $HOSTNAME:5000/twobombs/deploy-nvidia-docker
 docker push $HOSTNAME:5000/twobombs/deploy-nvidia-docker &
+
+docker build --no-cache -f Dockerfile2204-dev . -t twobombs/deploy-nvidia-docker:2204dev > deploy-nvidia-docker-2204dev && docker tag twobombs/deploy-nvidia-docker:2204dev $HOSTNAME:5000/twobombs/deploy-nvidia-docker:2204dev
+docker push $HOSTNAME:5000/twobombs/deploy-nvidia-docker:2204dev
+
 docker build --no-cache -f Dockerfile-dev . -t twobombs/deploy-nvidia-docker:dev > deploy-nvidia-docker-dev && docker tag twobombs/deploy-nvidia-docker:dev $HOSTNAME:5000/twobombs/deploy-nvidia-docker:dev
 docker push $HOSTNAME:5000/twobombs/deploy-nvidia-docker:dev
+
 docker build --no-cache -f Dockerfile-minimum . -t twobombs/deploy-nvidia-docker:minimum > deploy-nvidia-docker-minimum && docker tag twobombs/deploy-nvidia-docker:minimum $HOSTNAME:5000/twobombs/deploy-nvidia-docker:minimum
 docker push $HOSTNAME:5000/twobombs/deploy-nvidia-docker:minimum
