55851e06ac64bc9da4043d3b6ee31ad89145fc73
Aryan Blaauw <ablaauw@gmx.net>
2024-09-24 18:59:17 +0200

    Update cudacluster.sh

--- runfiles/cudacluster.sh
+++ runfiles/cudacluster.sh
@@ -9,19 +9,18 @@ docker build --no-cache -f Dockerfile . -t twobombs/cudacluster > cudacluster &&
 docker push $HOSTNAME:5000/twobombs/cudacluster &
 
 docker build --no-cache -f Dockerfile-dev . -t twobombs/cudacluster:dev > cudacluster:dev && docker tag twobombs/cudacluster:dev $HOSTNAME:5000/twobombs/cudacluster:dev
-docker push $HOSTNAME:5000/twobombs/cudacluster:dev &
+docker push $HOSTNAME:5000/twobombs/cudacluster:dev
 
 docker build --no-cache -f Dockerfile2204-dev . -t twobombs/cudacluster:2204dev > cudacluster:2204dev && docker tag twobombs/cudacluster:2204dev $HOSTNAME:5000/twobombs/cudacluster:2204dev
 docker push $HOSTNAME:5000/twobombs/cudacluster:2204dev
 
-docker build --no-cache -f Dockerfile-hpc . -t twobombs/cudacluster:hpc > cudacluster:hpc && docker tag twobombs/cudacluster:hpc $HOSTNAME:5000/twobombs/cudacluster:hpc
-docker push $HOSTNAME:5000/twobombs/cudacluster:hpc &
+docker build --no-cache -f Dockerfile-hpc . -t twobombs/cudacluster:hpc > cudacluster:hpc && docker tag twobombs/cudacluster:hpc $HOSTNAME:5000/twobombs/cudacluster:hpc && docker push $HOSTNAME:5000/twobombs/cudacluster:hpc &
 
-docker build --no-cache -f Dockerfile-samples . -t twobombs/cudacluster:samples > cudacluster:samples && docker tag twobombs/cudacluster:samples $HOSTNAME:5000/twobombs/cudacluster:samples
-docker push $HOSTNAME:5000/twobombs/cudacluster:samples &
+docker build --no-cache -f Dockerfile-samples . -t twobombs/cudacluster:samples > cudacluster:samples && docker tag twobombs/cudacluster:samples $HOSTNAME:5000/twobombs/cudacluster:samples && docker push $HOSTNAME:5000/twobombs/cudacluster:samples &
 
-docker build --no-cache -f Dockerfile-vulkan . -t twobombs/cudacluster:vulkan > cudacluster:vulkan && docker tag twobombs/cudacluster:vulkan $HOSTNAME:5000/twobombs/cudacluster:vulkan
-docker push $HOSTNAME:5000/twobombs/cudacluster:vulkan
+docker build --no-cache -f Dockerfile-vulkan . -t twobombs/cudacluster:vulkan > cudacluster:vulkan && docker tag twobombs/cudacluster:vulkan $HOSTNAME:5000/twobombs/cudacluster:vulkan && docker push $HOSTNAME:5000/twobombs/cudacluster:vulkan &
 
-docker build --no-cache -f Dockerfile-gaming . -t twobombs/cudacluster:gaming > cudacluster:gaming && docker tag twobombs/cudacluster:gaming $HOSTNAME:5000/twobombs/cudacluster:gaming
-docker push $HOSTNAME:5000/twobombs/cudacluster:gaming
+docker build --no-cache -f Dockerfile-gaming . -t twobombs/cudacluster:gaming > cudacluster:gaming && docker tag twobombs/cudacluster:gaming $HOSTNAME:5000/twobombs/cudacluster:gaming && docker push $HOSTNAME:5000/twobombs/cudacluster:gaming &
+
+docker stats
+tail -f /dev/null
