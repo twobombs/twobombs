dfdd6c022bf9468fa5609a642069949759b279a5
Aryan Blaauw <ablaauw@gmx.net>
2025-10-26 17:32:37 +0100

    Update thereminq-pqc.sh

--- runfiles/thereminq-pqc.sh
+++ runfiles/thereminq-pqc.sh
@@ -5,13 +5,13 @@ sleep 20
 
 cd thereminq-pqc
 
-docker build --no-cache -f Dockerfiles/Dockerfile . -t twobombs/thereminq-pqc > thereminq-pqc && docker tag twobombs/thereminq-pqc $HOSTNAME:5000/twobombs/thereminq-pqc
+docker build -f Dockerfiles/Dockerfile . -t twobombs/thereminq-pqc > thereminq-pqc && docker tag twobombs/thereminq-pqc $HOSTNAME:5000/twobombs/thereminq-pqc
 docker push $HOSTNAME:5000/twobombs/thereminq-pqc &
 
-docker build --no-cache -f Dockerfiles/Dockerfile-ssh . -t twobombs/thereminq-pqc:ssh > thereminq-pqc && docker tag twobombs/thereminq-pqc:ssh $HOSTNAME:5000/twobombs/thereminq-pqc:ssh
+docker build -f Dockerfiles/Dockerfile-ssh . -t twobombs/thereminq-pqc:ssh > thereminq-pqc && docker tag twobombs/thereminq-pqc:ssh $HOSTNAME:5000/twobombs/thereminq-pqc:ssh
 docker push $HOSTNAME:5000/twobombs/thereminq-pqc:ssh &
 
-docker build --no-cache -f Dockerfiles/Dockerfile-nginx . -t twobombs/thereminq-pqc:nginx > thereminq-pqc:nginx && docker tag twobombs/thereminq-pqc:nginx $HOSTNAME:5000/twobombs/thereminq-pqc:nginx
+docker build -f Dockerfiles/Dockerfile-nginx . -t twobombs/thereminq-pqc:nginx > thereminq-pqc:nginx && docker tag twobombs/thereminq-pqc:nginx $HOSTNAME:5000/twobombs/thereminq-pqc:nginx
 docker push $HOSTNAME:5000/twobombs/thereminq-pqc:nginx &
 
 docker stats
