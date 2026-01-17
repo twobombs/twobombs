0d41918a4ef907771518674299f8017f9cd9c090
Aryan Blaauw <ablaauw@gmx.net>
2026-01-17 13:56:57 +0100

    Update qrackmin.sh

--- runfiles/qrackmin.sh
+++ runfiles/qrackmin.sh
@@ -5,28 +5,28 @@ sleep 20
 
 cd qrackmin
 
-docker build -f dockerfiles/Dockerfile . -t twobombs/qrackmin > qrackmin && docker tag twobombs/qrackmin $HOSTNAME:5000/twobombs/qrackmin 
-docker push $HOSTNAME:5000/twobombs/qrackmin &
-docker build -f dockerfiles/Dockerfile-1804 . -t twobombs/qrackmin:1804 > qrackmin:1804 && docker tag twobombs/qrackmin:1804 $HOSTNAME:5000/twobombs/qrackmin:1804
-docker push $HOSTNAME:5000/twobombs/qrackmin:1804 &
-docker build -f dockerfiles/Dockerfile-2004 . -t twobombs/qrackmin:2004 > qrackmin:2004 && docker tag twobombs/qrackmin:2004 $HOSTNAME:5000/twobombs/qrackmin:2004
-docker push $HOSTNAME:5000/twobombs/qrackmin:2004 &
-docker build -f dockerfiles/Dockerfile-2204 . -t twobombs/qrackmin:2204 > qrackmin:2204 && docker tag twobombs/qrackmin:2204 $HOSTNAME:5000/twobombs/qrackmin:2204 
-docker push $HOSTNAME:5000/twobombs/qrackmin:2204 &
+docker build -f dockerfiles/Dockerfile . -t twobombs/thereminq-hpc > thereminq-hpc && docker tag twobombs/thereminq-hpc $HOSTNAME:5000/twobombs/thereminq-hpc 
+docker push $HOSTNAME:5000/twobombs/thereminq-hpc &
+docker build -f dockerfiles/Dockerfile-1804 . -t twobombs/thereminq-hpc:1804 > thereminq-hpc:1804 && docker tag twobombs/thereminq-hpc:1804 $HOSTNAME:5000/twobombs/thereminq-hpc:1804
+docker push $HOSTNAME:5000/twobombs/thereminq-hpc:1804 &
+docker build -f dockerfiles/Dockerfile-2004 . -t twobombs/thereminq-hpc:2004 > thereminq-hpc:2004 && docker tag twobombs/thereminq-hpc:2004 $HOSTNAME:5000/twobombs/thereminq-hpc:2004
+docker push $HOSTNAME:5000/twobombs/thereminq-hpc:2004 &
+docker build -f dockerfiles/Dockerfile-2204 . -t twobombs/thereminq-hpc:2204 > thereminq-hpc:2204 && docker tag twobombs/thereminq-hpc:2204 $HOSTNAME:5000/twobombs/thereminq-hpc:2204 
+docker push $HOSTNAME:5000/twobombs/thereminq-hpc:2204 &
 
-docker build -f dockerfiles/Dockerfile-aws . -t twobombs/qrackmin:aws > qrackmin:aws && docker tag twobombs/qrackmin:aws $HOSTNAME:5000/twobombs/qrackmin:aws && docker push $HOSTNAME:5000/twobombs/qrackmin:aws &
-docker build -f dockerfiles/Dockerfile-braket . -t twobombs/qrackmin:braket > qrackmin:braket && docker tag twobombs/qrackmin:braket $HOSTNAME:5000/twobombs/qrackmin:braket && docker push $HOSTNAME:5000/twobombs/qrackmin:braket &
-docker build -f dockerfiles/Dockerfile-cuda . -t twobombs/qrackmin:cuda > qrackmin:cuda && docker tag twobombs/qrackmin:cuda $HOSTNAME:5000/twobombs/qrackmin:cuda && docker push $HOSTNAME:5000/twobombs/qrackmin:cuda &
-docker build -f dockerfiles/Dockerfile-pocl . -t twobombs/qrackmin:pocl > qrackmin:pocl docker tag twobombs/qrackmin:pocl $HOSTNAME:5000/twobombs/qrackmin:pocl && docker push $HOSTNAME:5000/twobombs/qrackmin:pocl &
-docker build -f dockerfiles/Dockerfile-pyqrack . -t twobombs/qrackmin:pyqrack > qrackmin:pyqrack && docker tag twobombs/qrackmin:pyqrack $HOSTNAME:5000/twobombs/qrackmin:pyqrack && docker push $HOSTNAME:5000/twobombs/qrackmin:pyqrack &
-docker build -f dockerfiles/Dockerfile-qbdd . -t twobombs/qrackmin:qbdd > qrackmin:qbdd && docker tag twobombs/qrackmin:qbdd $HOSTNAME:5000/twobombs/qrackmin:qbdd && docker push $HOSTNAME:5000/twobombs/qrackmin:qbdd &
-docker build -f dockerfiles/Dockerfile-vcl . -t twobombs/qrackmin:vcl > qrackmin:vcl && docker tag twobombs/qrackmin:vcl $HOSTNAME:5000/twobombs/qrackmin:vcl
-docker push $HOSTNAME:5000/twobombs/qrackmin:vcl &
+docker build -f dockerfiles/Dockerfile-aws . -t twobombs/thereminq-hpc:aws > thereminq-hpc:aws && docker tag twobombs/thereminq-hpc:aws $HOSTNAME:5000/twobombs/thereminq-hpc:aws && docker push $HOSTNAME:5000/twobombs/thereminq-hpc:aws &
+docker build -f dockerfiles/Dockerfile-braket . -t twobombs/thereminq-hpc:braket > thereminq-hpc:braket && docker tag twobombs/thereminq-hpc:braket $HOSTNAME:5000/twobombs/thereminq-hpc:braket && docker push $HOSTNAME:5000/twobombs/thereminq-hpc:braket &
+docker build -f dockerfiles/Dockerfile-cuda . -t twobombs/thereminq-hpc:cuda > thereminq-hpc:cuda && docker tag twobombs/thereminq-hpc:cuda $HOSTNAME:5000/twobombs/thereminq-hpc:cuda && docker push $HOSTNAME:5000/twobombs/thereminq-hpc:cuda &
+docker build -f dockerfiles/Dockerfile-pocl . -t twobombs/thereminq-hpc:pocl > thereminq-hpc:pocl docker tag twobombs/thereminq-hpc:pocl $HOSTNAME:5000/twobombs/thereminq-hpc:pocl && docker push $HOSTNAME:5000/twobombs/thereminq-hpc:pocl &
+docker build -f dockerfiles/Dockerfile-pyqrack . -t twobombs/thereminq-hpc:pyqrack > thereminq-hpc:pyqrack && docker tag twobombs/thereminq-hpc:pyqrack $HOSTNAME:5000/twobombs/thereminq-hpc:pyqrack && docker push $HOSTNAME:5000/twobombs/thereminq-hpc:pyqrack &
+docker build -f dockerfiles/Dockerfile-qbdd . -t twobombs/thereminq-hpc:qbdd > thereminq-hpc:qbdd && docker tag twobombs/thereminq-hpc:qbdd $HOSTNAME:5000/twobombs/thereminq-hpc:qbdd && docker push $HOSTNAME:5000/twobombs/thereminq-hpc:qbdd &
+docker build -f dockerfiles/Dockerfile-vcl . -t twobombs/thereminq-hpc:vcl > thereminq-hpc:vcl && docker tag twobombs/thereminq-hpc:vcl $HOSTNAME:5000/twobombs/thereminq-hpc:vcl
+docker push $HOSTNAME:5000/twobombs/thereminq-hpc:vcl &
 
-docker build -f dockerfiles/Dockerfile-vcl-controller . -t twobombs/qrackmin:vcl-controller > qrackmin:vcl-controller && docker tag twobombs/qrackmin:vcl-controller $HOSTNAME:5000/twobombs/qrackmin:vcl-controller
-docker push $HOSTNAME:5000/twobombs/qrackmin:vcl-controller
-docker build -f dockerfiles/Dockerfile-vcl-pocl . -t twobombs/qrackmin:vcl-pocl > qrackmin:vcl-controller && docker tag twobombs/qrackmin:vcl-pocl $HOSTNAME:5000/twobombs/qrackmin:vcl-pocl
-docker push $HOSTNAME:5000/twobombs/qrackmin:vcl-pocl
+docker build -f dockerfiles/Dockerfile-vcl-controller . -t twobombs/thereminq-hpc:vcl-controller > thereminq-hpc:vcl-controller && docker tag twobombs/thereminq-hpc:vcl-controller $HOSTNAME:5000/twobombs/thereminq-hpc:vcl-controller
+docker push $HOSTNAME:5000/twobombs/thereminq-hpc:vcl-controller
+docker build -f dockerfiles/Dockerfile-vcl-pocl . -t twobombs/thereminq-hpc:vcl-pocl > thereminq-hpc:vcl-controller && docker tag twobombs/thereminq-hpc:vcl-pocl $HOSTNAME:5000/twobombs/thereminq-hpc:vcl-pocl
+docker push $HOSTNAME:5000/twobombs/thereminq-hpc:vcl-pocl
 
 
 
