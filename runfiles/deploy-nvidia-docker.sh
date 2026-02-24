eb91b63f15d808cf7ea3df01856c71aeb4e4b1bd
Aryan Blaauw <ablaauw@gmx.net>
2026-02-24 13:21:53 +0100

    Update deployment script for thereminq-desktop

--- runfiles/deploy-nvidia-docker.sh
+++ runfiles/deploy-nvidia-docker.sh
@@ -3,19 +3,10 @@
 # give docker time to spin up
 sleep 20
 
-cd deploy-nvidia-docker
+cd thereminq-desktop
 
-docker build -f Dockerfile . -t twobombs/deploy-nvidia-docker > deploy-nvidia-docker && docker tag twobombs/deploy-nvidia-docker $HOSTNAME:5000/twobombs/deploy-nvidia-docker
-docker push $HOSTNAME:5000/twobombs/deploy-nvidia-docker &
-
-docker build -f Dockerfile2204-dev . -t twobombs/deploy-nvidia-docker:2204dev > deploy-nvidia-docker-2204dev && docker tag twobombs/deploy-nvidia-docker:2204dev $HOSTNAME:5000/twobombs/deploy-nvidia-docker:2204dev
-docker push $HOSTNAME:5000/twobombs/deploy-nvidia-docker:2204dev &
-
-docker build -f Dockerfile-dev . -t twobombs/deploy-nvidia-docker:dev > deploy-nvidia-docker-dev && docker tag twobombs/deploy-nvidia-docker:dev $HOSTNAME:5000/twobombs/deploy-nvidia-docker:dev
-docker push $HOSTNAME:5000/twobombs/deploy-nvidia-docker:dev
-
-docker build -f Dockerfile-minimum . -t twobombs/deploy-nvidia-docker:minimum > deploy-nvidia-docker-minimum && docker tag twobombs/deploy-nvidia-docker:minimum $HOSTNAME:5000/twobombs/deploy-nvidia-docker:minimum
-docker push $HOSTNAME:5000/twobombs/deploy-nvidia-docker:minimum
+docker build -f Dockerfile . -t twobombs/thereminq-desktop > thereminq-desktop && docker tag twobombs/thereminq-desktop $HOSTNAME:5000/twobombs/thereminq-desktop
+docker push $HOSTNAME:5000/twobombs/thereminq-desktop &
 
 docker stats
 tail -f /dev/null
