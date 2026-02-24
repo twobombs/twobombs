e51e4f43fc1f4b0e75345d009cbec266d9630d95
Aryan Blaauw <ablaauw@gmx.net>
2026-02-24 13:19:38 +0100

    Replace qrackmin with thereminq-hpc in script

--- publish-thereminq-hpc.sh
+++ publish-thereminq-hpc.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:1804
-docker tag $HOSTNAME:5000/twobombs/qrackmin:1804 twobombs/qrackmin:1804
-docker push twobombs/qrackmin:1804 &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:1804
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:1804 twobombs/thereminq-hpc:1804
+docker push twobombs/thereminq-hpc:1804 &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:2004
-docker tag $HOSTNAME:5000/twobombs/qrackmin:2004 twobombs/qrackmin:2004
-docker push twobombs/qrackmin:2004 &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:2004
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:2004 twobombs/thereminq-hpc:2004
+docker push twobombs/thereminq-hpc:2004 &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:2204
-docker tag $HOSTNAME:5000/twobombs/qrackmin:2204 twobombs/qrackmin:2204
-docker push twobombs/qrackmin:2204 &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:2204
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:2204 twobombs/thereminq-hpc:2204
+docker push twobombs/thereminq-hpc:2204 &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:aws
-docker tag $HOSTNAME:5000/twobombs/qrackmin:aws twobombs/qrackmin:aws
-docker push twobombs/qrackmin:aws &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:aws
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:aws twobombs/thereminq-hpc:aws
+docker push twobombs/thereminq-hpc:aws &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:braket
-docker tag $HOSTNAME:5000/twobombs/qrackmin:braket twobombs/qrackmin:braket
-docker push twobombs/qrackmin:braket &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:braket
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:braket twobombs/thereminq-hpc:braket
+docker push twobombs/thereminq-hpc:braket &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:cuda
-docker tag $HOSTNAME:5000/twobombs/qrackmin:cuda twobombs/qrackmin:cuda
-docker push twobombs/qrackmin:cuda &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:cuda
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:cuda twobombs/thereminq-hpc:cuda
+docker push twobombs/thereminq-hpc:cuda &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:pocl
-docker tag $HOSTNAME:5000/twobombs/qrackmin:pocl twobombs/qrackmin:pocl
-docker push twobombs/qrackmin:pocl &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:pocl
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:pocl twobombs/thereminq-hpc:pocl
+docker push twobombs/thereminq-hpc:pocl &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:pyqrack
-docker tag $HOSTNAME:5000/twobombs/qrackmin:pyqrack twobombs/qrackmin:pyqrack
-docker push twobombs/qrackmin:pyqrack &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:pyqrack
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:pyqrack twobombs/thereminq-hpc:pyqrack
+docker push twobombs/thereminq-hpc:pyqrack &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:qbdd
-docker tag $HOSTNAME:5000/twobombs/qrackmin:qndd twobombs/qrackmin:qbdd
-docker push twobombs/qrackmin:qbdd &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:qbdd
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:qndd twobombs/thereminq-hpc:qbdd
+docker push twobombs/thereminq-hpc:qbdd &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:qiskit
-docker tag $HOSTNAME:5000/twobombs/qrackmin:qiskit twobombs/qrackmin:qiskit
-docker push twobombs/qrackmin:qiskit &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:qiskit
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:qiskit twobombs/thereminq-hpc:qiskit
+docker push twobombs/thereminq-hpc:qiskit &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:vcl
-docker tag $HOSTNAME:5000/twobombs/qrackmin:vcl twobombs/qrackmin:vcl
-docker push twobombs/qrackmin:vcl &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:vcl
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:vcl twobombs/thereminq-hpc:vcl
+docker push twobombs/thereminq-hpc:vcl &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:vcl-controller
-docker tag $HOSTNAME:5000/twobombs/qrackmin:vcl-controller twobombs/qrackmin:vcl-controller
-docker push twobombs/qrackmin:vcl-controller &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:vcl-controller
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:vcl-controller twobombs/thereminq-hpc:vcl-controller
+docker push twobombs/thereminq-hpc:vcl-controller &
 
-docker pull $HOSTNAME:5000/twobombs/qrackmin:vcl-pocl
-docker tag $HOSTNAME:5000/twobombs/qrackmin:vcl-pocl twobombs/qrackmin:vcl-pocl
-docker push twobombs/qrackmin:vcl-pocl &
+docker pull $HOSTNAME:5000/twobombs/thereminq-hpc:vcl-pocl
+docker tag $HOSTNAME:5000/twobombs/thereminq-hpc:vcl-pocl twobombs/thereminq-hpc:vcl-pocl
+docker push twobombs/thereminq-hpc:vcl-pocl &
 
