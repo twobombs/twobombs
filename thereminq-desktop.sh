53b0a8fbdd4f6047514833bb42bd4e75e755b31a
Aryan Blaauw <ablaauw@gmx.net>
2026-08-29 11:24:16 +0200

    Add support for mesa-rusticl Docker build

--- thereminq-desktop.sh
+++ thereminq-desktop.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # build and push
-# give docker time to spin up
 
 cd thereminq-desktop
 
 docker build -f Dockerfile . -t twobombs/thereminq-desktop
 docker push twobombs/thereminq-desktop
 
+docker build -f Dockerfile.mesa-rusticl . -t twobombs/thereminq-desktop:mesa
+docker push twobombs/thereminq-desktop:mesa
